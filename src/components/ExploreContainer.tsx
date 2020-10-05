7799af8f859bfdfd9298a0d6a706c2c466935f8d
Nicholas Hyatt <nicholaspaulhyatt@gmail.com>
2020-10-05 14:58:48 -0500

    Use env vars to customize app

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { Capacitor } from '@capacitor/core';
 import './ExploreContainer.css';
 
-interface ContainerProps {
-  name: string;
-}
-
-const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+const ExploreContainer: React.FC = () => {
+  const ciTrigger = process.env.REACT_APP_CI_TRIGGER || 'GitLab';
+  const platform = Capacitor.getPlatform();
+  const binaryType = platform === 'ios' ? 'IPA' : platform === 'android' ? 'APK' : 'website'
   return (
     <div className="container">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <strong>This {binaryType} was built by Appflow</strong>
+      { ciTrigger && (
+        <p>Triggered by {ciTrigger}
+          <br/>
+            <img src={`assets/appflow.png`} height="100px" />
+            <img src={`assets/${ciTrigger.toLowerCase()}.png`} height="100px" />
+        </p>
+      )}
     </div>
   );
 };
