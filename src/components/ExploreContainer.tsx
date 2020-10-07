2f86e6b7a02f4265a32130b88abf578e721a9723
Nicholas Hyatt <nicholaspaulhyatt@gmail.com>
2020-10-07 12:48:03 -0500

    changed icon for live update

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
-import { Capacitor } from '@capacitor/core';
 import './ExploreContainer.css';
 
 const ExploreContainer: React.FC = () => {
-  const ciTrigger = process.env.REACT_APP_CI_TRIGGER || 'Ionic CLI';
-  const platform = Capacitor.getPlatform();
-  const binaryType = platform === 'ios' ? 'IPA' : platform === 'android' ? 'APK' : 'website'
   return (
     <div className="container">
-      <strong>This {binaryType} was built by Appflow</strong>
-      { ciTrigger && (
-        <p>Triggered by {ciTrigger}
+      <strong>This Live Update was built by Appflow</strong>
+        <p>Shipped by Ionic
           <br/>
             <img src={`assets/appflow.png`} height="100px" alt="" />
-            <img src={`assets/${ciTrigger.toLowerCase()}.png`} height="100px" alt="" />
+            <img src={`assets/ionic.png`} height="100px" alt="" />
         </p>
-      )}
     </div>
   );
 };
